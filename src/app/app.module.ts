1f1e7cba6f6fb805173d4cefd337238c7bbf916e
samkelham <sam@kelham.co>
2018-03-23 10:17:07 +0700

    feat(appModule): add firebase to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GroupNamesComponent } from './components/group-names/group-names.component';
+import { NameComponent } from './components/name/name.component';
+import { AddNewNameComponent } from './components/add-new-name/add-new-name.component';
+import { CreateNewPartyComponent } from './components/create-new-party/create-new-party.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {environment} from '../environments/environment';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GroupNamesComponent,
+    NameComponent,
+    AddNewNameComponent,
+    CreateNewPartyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyARFREZhfY1bGy5xIaCcjlSoBc5MiQOD8c',
+    authDomain: 'world-cup-sweepstake.firebaseapp.com',
+    databaseURL: 'https://world-cup-sweepstake.firebaseio.com',
+    projectId: 'world-cup-sweepstake',
+    storageBucket: 'world-cup-sweepstake.appspot.com',
+    messagingSenderId: '587705123967'
+  }
 };
